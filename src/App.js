1aced306f63bbf2731b717331318c1f469fe541c
ksahinbedirhan <ksahinbedirhan@gmail.com>
2023-09-25 15:51:42 +0300

    değişiklik

--- src/App.js
+++ src/App.js
@@ -1,51 +1,44 @@
-
 import './App.css';
-import Card from './components/Card';
 import { useState } from 'react'
 
-// const App = () => {
+const App = () => {
+  const [visible, setVisible] = useState(false)
+  const [color, setColor] = useState("pink")
 
-//   const [visible, setVisible] = useState(true)
-//   const [color, setColor] = useState("white")
+  const toggleColor = () => {
+    setColor(color === "pink" ? "lightblue" : "pink")
+    if (!visible) {
+      setVisible(true) 
+    }
+  }
 
-//   return (
-//     <div>
-//       <div>
-//         <button className="sagda-goster" onClick={() => setVisible(!visible)}>Aç/Kapat</button>
-//         <button className="sagda-goster" onClick={() => setColor(color === "white" ? "red" :"white")}>Kırmızı/Beyaz</button>
-//       </div>
-//       <div className="card-group">
-//         {visible && <div >
-//           <div style={{ backgroundColor: color, border: "1px solid black", marginLeft: 5, marginTop: 5, padding: 10 }}>
-//             <div>
-//               <p>Ad: Sadik</p>
-//               <p>Soyad: KRAL</p>
-//               <p>Yaş: Bilinmiyor</p>
-//               <p>Cinsiyet: Söylemeye gerek dahi yok ERKEKKK</p>
-//               <p>Memleket: Yiğidin harman olduğu yer</p>
-//             </div>
-//           </div>
-//         </div>}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-
- const App = () => {
- const [todo, setTodo] = useState([])
- const [value, setValue] = useState('')
-console.log(todo)
- return (     <div>
-       <input type='text' value={value} onChange={(e) => setValue(e.target.value)}></input>      <button onClick={() => setTodo([...todo, value])}>
-        Ekle
-      </button>
-    {todo.map((item, index)=><div key={index}>{item}</div>)}
+  return (
+    <div>
+      <div>
+        <button style={{ border: "1px solid black", marginLeft: 5, marginTop: 5, padding: 10 }} onClick={() => setVisible(!visible)}>Aç/Kapat</button>
+        <br />
+        <br />
+        <button style={{ border: "1px solid black", marginLeft: 5, marginTop: 5, padding: 10 }} onClick={toggleColor}>Pembe/Mavi</button>
+        <br />
+        <br />
+      </div>
+      <div className="card-group">
+        {visible && (
+          <div>
+            <div style={{ backgroundColor: color, border: "1px solid black", marginLeft: 5, marginTop: 5, padding: 10 }}>
+              <div>
+                <p>Ad: Sadık</p>
+                <p>Soyad: Bey</p>
+                <p>Yaş: Değişkenlik Gösteriyor</p>
+                <p>Cinsiyet: Erkek</p>
+                <p>Memleket: Çorum</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
- }
+}
 
 export default App
-
